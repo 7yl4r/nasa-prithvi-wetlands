db5e4cb6000b1c4c32e10d67524d3014460ceb00
Tylar <murray.tylar@gmail.com>
2025-12-11 13:53:33 -0500

    fix script

--- py/download_seagrass_and_mangrove.py
+++ py/download_seagrass_and_mangrove.py
@@ -1,6 +1,7 @@
 """
 Creates .csv files with taxa occurrences from OBIS and GBIF
 """
+import sys
 import pandas as pd
 
 from dwc_tools.get_gbif_data import get_gbif_data
@@ -23,24 +24,29 @@ area_bbox = [-85.9, 29.7, -85.3, 30.1]
 gbif_df = get_gbif_data(seagrass_names, area_bbox=area_bbox)
 obis_df = get_obis_occurrences_by_taxaids(seagrass_aphia_ids, area_bbox=area_bbox)
 
+print(f"GBIF data: {gbif_df.columns}")
+print(f"OBIS data: {obis_df.columns}")
+
 # combine OBIS and GBIF data
 combined_df = pd.concat([obis_df, gbif_df], ignore_index=True)
-combined_df = combined_df.drop_duplicates().dropna(subset=['lat', 'lon', 'time']).reset_index(drop=True)
-print("Combined OBIS + GBIF data:", combined_df.shape)
-combined_df.head()
+
+print("Combined OBIS + GBIF data before cleaning:", combined_df.head())
+
 
 # === clean & preprocess
-combined_df = combined_df.drop_duplicates()
+combined_df = combined_df.drop_duplicates(
+    subset=['decimalLatitude', 'decimalLongitude', 'eventDate']
+).dropna(
+    subset=['decimalLatitude', 'decimalLongitude', 'eventDate']
+).reset_index(drop=True)
 
-combined_df = combined_df.dropna(subset=['lat', 'lon', 'time'])
+print("Combined OBIS + GBIF data after cleaning:", combined_df.head())
 
-combined_df['time'] = pd.to_datetime(combined_df['time'], errors='coerce')
-combined_df = combined_df.dropna(subset=['time'])
+combined_df['time'] = pd.to_datetime(combined_df['eventDate'], errors='coerce')
 combined_df['year'] = combined_df['time'].dt.year
 
-combined_df = combined_df[combined_df['year'] >= 2000]
-
-combined_df = combined_df.reset_index(drop=True)
+#combined_df = combined_df[combined_df['year'] >= 2000]
+#combined_df = combined_df.reset_index(drop=True)
 
 # === save to csv
 fname = "seagrass_occurrences_st_andrew_bay.csv"
